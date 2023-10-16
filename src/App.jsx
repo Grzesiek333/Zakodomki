99f8fc7e282d50e77524b60b0a569afeb47ed11a
Grzesiek333 <grzesiek333@gmail.com>
2023-10-16 21:12:20 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+}from 'react-router-dom'; 
+
+function App() {
+   return (
+    <>
+      <h1>Zakodomki</h1>
+      <div>abc</div>
+    </>
+  )
+}
+
+export default App
