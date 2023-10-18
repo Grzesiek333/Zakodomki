bd21b1a33aba89f238ba7efbb46d6f75b12a1a4b
Grzesiek333 <grzesiek333@gmail.com>
2023-10-18 12:56:48 +0200

    install_primereact

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import {
   BrowserRouter as Router,
   Routes,
   Route
-}from 'react-router-dom'; 
+} from 'react-router-dom'; 
+
+import './calendar.jsx';
+import InlineDemo from './calendar.jsx';
 
 function App() {
    return (
     <>
       <h1>Zakodomki</h1>
       <div>abc</div>
+      <InlineDemo />
     </>
   )
 }
