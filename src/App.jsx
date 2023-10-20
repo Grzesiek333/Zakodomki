b0b1a6d6243be0486d511a1d80e0b5c68ebf9c70
Grzesiek333 <grzesiek333@gmail.com>
2023-10-20 18:28:07 +0200

    dodawanie dat do supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.scss'
-import { 
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'; 
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './calendar.jsx';
-import InlineDemo from './calendar.jsx';
-import CalendarComponent from './import_booking'
+import InlineDemo from "./components/calendar";
+// import CalendarComponent from './import_booking'
+import SignUp from "./components/signup";
+import SignIn from "./components/signin";
+import Home from "./components/home";
+import Reservations from "./components/reservations";
+
 
 function App() {
-   return (
-    <>
-      <h1>Zakodomki</h1>
-      <div>abc</div>
-      <InlineDemo />
-      <CalendarComponent />
-    </>
-  )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calendar" element={<InlineDemo />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/reservations" element={<Reservations />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/signup.jsx
@@ -0,0 +1,32 @@
+import supabase from "../services/supabase";
+
+export default function SignUp() {
+
+  async function onSignUp(e) {
+    e.preventDefault();
+
+    const formElements = e.currentTarget.elements;
+    console.log(formElements);
+
+    let { data, error } = await supabase.auth.signUp({
+      email: formElements[0].value,
+      password: formElements[1].value,
+    });
+
+    if (!error) {
+      console.log('user created successfully');
+      console.log(data);
+      return;
+    }
+
+    console.error('something went wrong', error);
+  }
+
+  return (
+    <form onSubmit={onSignUp}>
+      <input placeholder="email" />
+      <input placeholder="password" type="password"  />
+      <button>Sign up</button>
+    </form>
+  )
+}
