f8e63022baa3f53296869c21c6043847718b8467
Grzesiek333 <grzesiek333@gmail.com>
2023-11-02 00:59:37 +0100

    add_contact_name

--- src/components/form_input.jsx
+++ src/components/form_input.jsx
@@ -5,17 +5,36 @@ import supabase from '../services/supabase';
 
 function FormInput() {
   const [message, setMessage] = useState([]);
-  const [email, setEmail] = useState([]);
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [emailError, setEmailError] = useState('');
+
+  function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
 
   async function onSaveMessage(e) {
     e.preventDefault();
 
     const formElements = e.currentTarget.elements;
+    const emailValue = formElements[1].value;
+
+    if (!validateEmail(emailValue)) {
+      setEmailError('Podaj poprawny adres email.');
+      return;
+    }
 
     const { data, error } = await supabase
       .from('contact_message')
-      .insert([{ message: formElements[1].value, email: formElements[0].value }])
+      .insert([
+        {
+          message: formElements[2].value,
+          email: formElements[1].value,
+          name: formElements[0].value,
+        }
+      ])
       .select();
 
     if (!error) {
@@ -23,7 +42,7 @@ function FormInput() {
       console.log(data);
 
       setMessage(prev => [...prev, data[0]]);
-      setEmail(prev => [...prev, data[0]]);
+      setEmailError('');
       setSubmitted(true); // Ustaw potwierdzenie wysłania wiadomości
 
       return;
@@ -40,16 +59,30 @@ function FormInput() {
         </div>
       ) : (
         <Form onSubmit={onSaveMessage}>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInputName">
+            <Form.Label>Imię i nazwisko:</Form.Label>
+            <Form.Control
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInputEmail">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="przykład@wp.pl"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p style={{ color: 'red' }}>{emailError}</p>
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Label>Treść wiadomości:</Form.Label>
+            <Form.Control as="textarea" rows={5} style={{ width: '400px' }} />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            Wyślij
           </Button>
         </Form>
       )}
