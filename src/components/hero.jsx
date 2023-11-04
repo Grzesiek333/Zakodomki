520ad7bdd91a2258a82b178576897b0ab3a256aa
Grzesiek333 <grzesiek333@gmail.com>
2023-11-04 14:48:02 +0100

    ScrollToSection_GridCards

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,6 +3,21 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Link } from 'react-router-dom';
 
+function ScrollToSection_GridCards() {
+  const scrollToSection = () => {
+    const section = document.getElementById('sekcja-gridcards');
+    if (section) {
+      const offset = section.offsetTop - 50; // Odejmij 50 pikseli od wysokości sekcji
+      window.scrollTo({ top: offset, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <a className='btn btn-outline-light btn-lg' role='button' onClick={scrollToSection}>
+      Zapraszamy
+    </a>
+  );
+}
 export default function HeroImage() {
   return (
     <div style={{ paddingLeft: 0 }}>
@@ -14,12 +29,11 @@ export default function HeroImage() {
           <div className='d-flex justify-content-center align-items-center h-100'>
             <div className='text-white'>
               <h1 className='mb-3'>Zakodomki</h1>
-              <h4 className='mb-3'>Regionalne domki w Kościelisku</h4>                          
-              <a className='btn btn-outline-light btn-lg' role='button'href="#sekcja-gridcards">
-                Zapraszamy
-              </a>              
+              <h4 className='mb-3'>Regionalne domki w Kościelisku</h4> 
+              <ScrollToSection_GridCards />                         
+
             </div>
-          </div>
+          </div>  
         </div>
       </div>
     </div>
