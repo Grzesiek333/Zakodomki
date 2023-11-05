37a42a20b3537c8ee4f037554569d7e2f8845884
Grzesiek333 <grzesiek333@gmail.com>
2023-11-05 16:35:47 +0100

    media_aboutus_correct

--- src/aboutus.jsx
+++ src/aboutus.jsx
@@ -11,10 +11,10 @@ export default function AboutUs() {
       <div style= {{width: '48%', textAlign: 'left'}}>ZAKODOMKI to domków i apartamentów łączących wygodę, nowoczesność i tradycje góralską. W trakcie procesu budowy i urządzania udało się nam stworzyć niezapomniany i wyjątkowy klimat z połączenia stylu zakopiańskiego z prawdziwą góralską architekturą. Zlokalizowane są w spokojnej dzielnicy Kościeliska. Zaledwie 4 km do centrum Zakopanego. Przy domku jest zielony ogród, grill. Z okien naszych góralskich domków rozpościera się wspaniały widok na Giewont i Czerwone Wierchy. Malownicze widoki na wszystkie górskie szczyty gwarantują niezapomniane wrażenia. Urządzone są w podobnym stylu łącząc wygodę i góralski klimat drewnianej chałupy. Wysoki standard, komfort i przestronność dają poczucie swobody i domowej atmosfery.      
       </div>
       <div style={{width: '48%', float: 'left'}}>
-        <img src="public/media/aboutus/1.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
-        <img src="public/media/aboutus/2.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
-        <img src="public/media/aboutus/3.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
-        <img src="public/media/aboutus/4.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
+        <img src="/media/aboutus/1.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
+        <img src="/media/aboutus/2.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
+        <img src="/media/aboutus/3.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
+        <img src="/media/aboutus/4.JPG" style={{minWidth: '150px', width: '50%', padding: '5px'}}/>
       </div>
       </div>
       <Footer />
