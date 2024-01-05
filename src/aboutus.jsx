35bc0d8b118aa006439481ec7459bcec0f48e1f4
Grzesiek333 <grzesiek333@gmail.com>
2024-01-05 13:47:25 +0100

    aboutus_text

--- src/aboutus.jsx
+++ src/aboutus.jsx
@@ -11,7 +11,7 @@ export default function AboutUs() {
         <div style={{ width: '100%', textAlign: 'left', overflow: 'hidden' }}>         
           <img src="/media/aboutus/1.JPG" style={{ minWidth: '150px', width: '50%', margin: '5px', float: 'right', borderRadius: '10px' }} />
           <div style={{ textAlign: 'left'}}>
-            ZAKODOMKI to domków i apartamentów łączących wygodę, nowoczesność i tradycje góralską. W trakcie procesu budowy i urządzania udało się nam stworzyć niezapomniany i wyjątkowy klimat z połączenia stylu zakopiańskiego z prawdziwą góralską architekturą.
+            ZAKODOMKI to domki i apartamenty łączące wygodę, nowoczesność i tradycje góralską. W trakcie procesu budowy i urządzania udało się nam stworzyć niezapomniany i wyjątkowy klimat z połączenia stylu zakopiańskiego z prawdziwą góralską architekturą.
             <br/>
              Zlokalizowane są w spokojnej dzielnicy Kościeliska. Zaledwie 4 km do centrum Zakopanego. Przy domku jest zielony ogród, grill. Z okien naszych góralskich domków rozpościera się wspaniały widok na Giewont i Czerwone Wierchy. Malownicze widoki na wszystkie górskie szczyty gwarantują niezapomniane wrażenia. Urządzone są w podobnym stylu łącząc wygodę i góralski klimat drewnianej chałupy. Wysoki standard, komfort i przestronność dają poczucie swobody i domowej atmosfery.
             <br/>
