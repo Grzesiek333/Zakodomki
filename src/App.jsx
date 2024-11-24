b1d558907ae1d5a4761e0cc37a698f7a32e67d1e
Grzesiek333 <grzesiek333@gmail.com>
2024-11-24 09:42:13 +0100

    delete_supabase(login,reservaction)

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import "./scss/App.scss";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// import CalendarComponent from './import_booking'
-import SignUp from "./signup";
-import SignIn from "./signin";
+
+
 import Home from "./home";
-import Reservations from "./reservations";
 import Szalas from "./szalas";
 import Pyzdra from "./pyzdra";
 import Tatrala from "./tatrala";
@@ -20,10 +18,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />        
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/" element={<Home />} />
         <Route path="/szalas" element={<Szalas />} />
         <Route path="/pyzdra" element={<Pyzdra />} />
         <Route path="/tatrala" element={<Tatrala />} />
